e4bce0ca97ab75136a8642e014fb95507837919c
Najifa Montaha Artika <najifamontahaartika@gmail.com>
2024-09-23 21:04:50 +0600

    changing a file nama and changing some js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const cost = document.getElementById('motMoney')
+const money = document.getElementById('Donate_money')
+const DonateBtn = document.getElementById('Donate_btn')
+DonateBtn.addEventListener('click', function(){
+    // if(DonateBtn > money){
+    //     alert('you have not enough money')
+    // }
+
+    // else{
+    //    alert('your money added succesfully')
+    // }
+
+    const totalAmount = DonateBtn + money;
+    const totalCost = totalAmount - cost;
+
+    const plus = input_tag_1 + Donate_btn;
+    const give = plus + Donate_money;
+    const mainus = motMoney - give;
+})
